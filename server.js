1b1c18c3dc3d47364ebe585f2e637507af562a28
mattwills09 <mattwills09@gmail.com>
2019-05-21 22:00:37 -0400

    update heroku hosting

--- server.js
+++ server.js
@@ -22,9 +22,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to Mongo DB ============
-mongoose.connect("mongodb://localhost/scrapetastic", { useNewUrlParser: true });
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapetastic";
+
+mongoose.connect("mongodb://localhost/scrapetastic", { useNewUrlParser: true });
+
 
 mongoose.connect(MONGODB_URI);
 
